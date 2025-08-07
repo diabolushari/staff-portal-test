ce8df655ee6a4f80bc17b2cd1e939e0b7da8c177
sreyas-xocortx <sreyas@intuonfx.com>
2025-08-07 07:19:57 +0530

    interface added

--- resources/js/interfaces/paramater_service.ts
+++ resources/js/interfaces/paramater_service.ts
@@ -2,3 +2,27 @@ export interface SystemModule {
   id: number
   name: string
 }
+
+export interface ParameterDomain {
+  id: number
+  name: string
+  description: string
+  domainCode: string
+  managedByModule: number
+}
+
+export interface ParameterDefinition {
+  id: number
+  name: string
+  description: string
+  domainCode: string
+  managedByModule: number
+}
+
+export interface ParameterValue {
+  id: number
+  name: string
+  description: string
+  domainCode: string
+  managedByModule: number
+}
