2d66f8a6733e0eeb34fa8ce3082f9c85efbcbc50
thejzain <thejzain@gmail.com>
2025-09-12 10:11:18 +0530

    cannot use meter that is already in the table

--- app/Http/Controllers/Metering/MeterConnectionRelCreateController.php
+++ app/Http/Controllers/Metering/MeterConnectionRelCreateController.php
@@ -25,13 +25,19 @@ class MeterConnectionRelCreateController extends Controller
         }
 
         $meters = $this->meterService->listMeters();
+        $meterRelations = $this->meterConnectionRelService->listMeterConnectionRels();
+        $meterRelationIds = array_column($meterRelations->data, 'meter_id');
+        $unrelatedMeters = array_filter($meters->data, function ($meter) use ($meterRelationIds) {
+            return ! in_array($meter['meter_id'], $meterRelationIds);
+        });
+
         $useCategory = $this->parameterValueService->getParameterValues(1, 100, null, 'Meter', 'Use Category');
         $meterStatus = $this->parameterValueService->getParameterValues(1, 100, null, 'Meter', 'Status');
         $changeReason = $this->parameterValueService->getParameterValues(1, 100, null, 'Meter', 'Change Reason');
 
         return Inertia::render('Connections/ConnectMeter', [
             'connection_id' => $Id,
-            'meters' => $meters->data,
+            'meters' => $unrelatedMeters,
             'useCategory' => $useCategory->data,
             'meterStatus' => $meterStatus->data,
             'changeReason' => $changeReason->data,
