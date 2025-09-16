051ad3adc85f302ce4d1dd1a9ad174a2a878c076
sreyas-xocortx <sreyas@intuonfx.com>
2025-09-16 10:43:47 +0530

    paramters search fixed

--- app/Http/Controllers/Parameter/ParameterDefinitionController.php
+++ app/Http/Controllers/Parameter/ParameterDefinitionController.php
@@ -24,9 +24,10 @@ class ParameterDefinitionController extends Controller
         $page = $request->input('page', 1);
         $pageSize = $request->input('page_size', 10);
         $domainName = $request->input('domain_name');
+        $moduleName = $request->input('module_name');
         $search = $request->input('search');
         $domainsResponse = $this->parameterDomainService->getParameterDomains($page, $pageSize, null, null);
-        $response = $this->parameterDefinitionService->getParameterDefinitions($page, $pageSize, $domainName, $search);
+        $response = $this->parameterDefinitionService->getParameterDefinitions($page, $pageSize, $domainName, $moduleName, $search);
 
         if ($response->hasError()) {
             return $response->error ?? redirect()->back()->with([
